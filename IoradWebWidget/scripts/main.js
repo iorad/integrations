21680d2fb53d13bc9c448455b2702542b34944f5
Daniel Sun <hjdaniel.sun@gmail.com>
2015-06-21 23:26:28 +1000

    Restructure iorad web widget so that we can support desk.com

--- IoradWebWidget/scripts/main.js
+++ IoradWebWidget/scripts/main.js
@@ -11,29 +11,3 @@ var ioradWebWidget = (function (module, undefined) {
 
   return module;
 })(ioradWebWidget || {});
-
-ioradWebWidget.templates = {
-  // main layout for iorad widget control. At the moment, this widget control works best with the new freshtheme.
-  mainLayout: function () {
-    var template = JST['templates/mainLayout.hbs'];
-    return template();
-  },
-
-  // This modal shows up when there is a new fresh desk article created successfully. Pass in the url as parameter.
-  freshDeskModal: function (article, title) {
-    var template = JST['templates/modalTemplate.hbs'];
-    return template({article: article, articleTitle: title});
-  },
-
-  // This template displays all available categories as options.
-  categoryOptionTemplate: function (categories) {
-    var template = JST['templates/freshdeskCategoryList.hbs'];
-    return template({ categories: categories });
-  },
-
-  // This template displays all folders as select/options.
-  folderListTemplate: function (categories) {
-    var template = JST['templates/freshdeskFolderList.hbs'];
-    return template({ categories: categories });
-  }
-};
--- IoradWebWidget/scripts/templates/handlebars/templates.js
+++ IoradWebWidget/scripts/templates/handlebars/templates.js
@@ -1,6 +1,6 @@
 this["JST"] = this["JST"] || {};
 
-this["JST"]["templates/freshdeskCategoryList.hbs"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+this["JST"]["templates/freshdesk/freshdeskCategoryList.hbs"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
   return "<option value=\""
@@ -14,7 +14,7 @@ this["JST"]["templates/freshdeskCategoryList.hbs"] = Handlebars.template({"1":fu
   return ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.categories : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
 
-this["JST"]["templates/freshdeskFolderList.hbs"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+this["JST"]["templates/freshdesk/freshdeskFolderList.hbs"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var stack1, helper;
 
   return "<select id=\""
@@ -36,11 +36,11 @@ this["JST"]["templates/freshdeskFolderList.hbs"] = Handlebars.template({"1":func
   return ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.categories : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
 
-this["JST"]["templates/mainLayout.hbs"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+this["JST"]["templates/freshdesk/mainLayout.hbs"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "<section id=\"ioradWidget\" class=\"content rounded-6 iorad-widget hide-in-mobile\" title=\"Iorad Tutorial Widget\">\r\n    <div id=\"tutorialLocation\" class=\"widget-layout\">\r\n        <div class=\"widget-header\"><h2 class=\"heading\">Create a Solution</h2></div>\r\n        <div class=\"widget-location-selector\"><div class=\"selector-label\"><h4>Categories</h4></div><div><select id=\"categorySelector\"></select></div></div>\r\n        <div class=\"widget-location-selector\"><div class=\"selector-label\"><h4>Solutions</h4></div><div id='foldersList'></div></div>\r\n        <div id=\"control\">\r\n            <a id='newTutorialBtn' class='btn btn-iorad-widget' title='open IORAD editor' href='#'>ADD</a>\r\n        </div>\r\n    </div>\r\n</section>\r\n<style>\r\n    .invisible-options {\r\n        display: none;\r\n    }\r\n\r\n    .iorad-widget {\r\n        border-bottom: 1px solid #e5e5e5;\r\n    }\r\n\r\n        .iorad-widget div {\r\n            display: inline-block;\r\n        }\r\n\r\n    #control {\r\n        float: right;\r\n        position: relative;\r\n    }\r\n\r\n    .widget-layout {\r\n        width: 98%;\r\n    }\r\n\r\n    .widget-header {\r\n        padding-right: 2em;\r\n    }\r\n\r\n    .widget-location-selector {\r\n        padding-right: 2em;\r\n        height: 30px;\r\n    }\r\n\r\n        .widget-location-selector .selector-label {\r\n            padding-right: 1em;\r\n        }\r\n\r\n    .btn-iorad-widget {\r\n        color: #006063;\r\n        border-color: #006063;\r\n        font-weight: bold;\r\n        background-color: white;\r\n        background-image: none;\r\n        text-shadow: none;\r\n        width: 112px;\r\n        line-height: 20px;\r\n        margin: 0 auto;\r\n        display: block;\r\n    }\r\n\r\n    .iorad-widget-modal {\r\n        width: 28%;\r\n        font-size: 16px;\r\n    }\r\n</style>\r\n<script type='text/javascript' src='//iorad.com/server/assets/js/iorad.js'></script>\r\n";
 },"useData":true});
 
-this["JST"]["templates/modalTemplate.hbs"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+this["JST"]["templates/freshdesk/modalTemplate.hbs"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1, helper, alias1=this.escapeExpression, alias2=this.lambda;
 
   return "<div class=\"modal hide fade iorad-widget-modal\" role=\"dialog\" id=\"successModal\" aria-hidden=\"true\" style=\"display:none;\">\r\n    <div class=\"modal-header\"></div>\r\n    <div class=\"modal-body\">\r\n        <div id=\"successMsg\">\r\n            The solution <b>"
