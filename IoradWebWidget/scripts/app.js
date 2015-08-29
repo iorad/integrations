f54167b6ce911acd2a4ecd05db25f11558ed870f
Daniel Sun <hjdaniel.sun@gmail.com>
2015-08-29 20:34:36 +1000

    Make freshdesk widget run on customized domain support portals.

--- IoradWebWidget/scripts/app.js
+++ IoradWebWidget/scripts/app.js
@@ -1,7 +1,5 @@
-﻿if (ioradWebWidget.util.common.isFreshdeskKnowledgebase()) {
-  ioradWebWidget.freshdesk.runApp(jQuery, window);
-}
+﻿//if (ioradWebWidget.util.common.isFreshdeskKnowledgebase()) {
+//  ioradWebWidget.freshdesk.runApp(jQuery, window);
+//}
 
-if (ioradWebWidget.util.common.isDeskKnowledgebase()) {
-  ioradWebWidget.desk.runApp(jQuery, window);
-}
+ioradWebWidget.freshdesk.runApp(jQuery, window);
