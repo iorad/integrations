ebc2250958d73969bbd2b35007d1292f365d5493
Daniel Sun <hjdaniel.sun@gmail.com>
2015-10-16 00:11:34 +1100

    initialize iorad and hook up button click event as well as editor close event.

--- IoradWebWidget/scripts/main/uservoice/app.js
+++ IoradWebWidget/scripts/main/uservoice/app.js
@@ -1,7 +1,6 @@
 ﻿ioradWebWidget.uservoice = (function (module, utils, $, win) {
 
   var loadContent = function () {
-
     $.ajax(utils.uservoice.listTopics())
      .complete(function (data) {
        $(".widget-body").prepend(ioradWebWidget.templates.uservoiceTemplates.widgetBodyTemplate(data.responseJSON.topics));
@@ -14,6 +13,35 @@
     } else {
       loadContent();
     }
+
+    var iorad = win.iorad || {};
+
+    iorad.init({ env: utils.common.ioradEnv(), pluginType: utils.uservoice.ioradPluginType }, function () {
+      var t = 0;
+
+      // register events
+      $("#newTutorialBtn").click(function () {
+        $("body").addClass("iorad-loading");
+        iorad.createTutorial();
+        t = setTimeout(function () {
+          $("body").removeClass("iorad-loading");
+        }, 5000);
+        $("#iorad-editor").off().load(function () {
+          $("body").addClass("iorad-open");
+          clearTimeout(t);
+        });
+      });
+
+      iorad.on("editor:close", function (tutorialParams) {
+        $("body").removeClass("iorad-open iorad-loading");
+        clearTimeout(t);
+        var iframeHTML = iorad.getEmbeddedPlayerUrl(tutorialParams.uid,
+              tutorialParams.tutorialId, tutorialParams.tutorialTitle),
+          $tutorialIframe = $(iframeHTML),
+          topicId = $("#topicSelector").val();
+
+      });
+    });
   };
 
   module.runApp = function () {
